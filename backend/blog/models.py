663d66abc4836a7a33fb1979bd665c9f584ad1b3
Fang Jiaan <fduodev@gmail.com>
2015-09-05 01:38:06 +0800

    Add foreign keys to category & comment table Change test database to postgresql

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -9,6 +9,11 @@ class BlogCategory(db.Model):
     title = db.Column(db.String(128), unique=True)
     order = db.Column(db.SmallInteger, default=0)
 
+    parent_id = db.Column(db.Integer, db.ForeignKey('blog_category.id'), index=True)
+    parent = db.relationship(lambda: BlogCategory,
+                             remote_side=id,
+                             backref=db.backref('sub_categories', lazy='dynamic'))
+
     __mapper_args__ = {
         'order_by': order
     }
@@ -24,8 +29,8 @@ class BlogPost(db.Model):
     created = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
     modified = db.Column(db.DateTime, server_default=db.func.now())
 
-    category_id = db.Column(db.Integer, db.ForeignKey(BlogCategory.id))
-    category = db.relationship(BlogCategory, backref=db.backref('posts', lazy='dynamic'))
+    category_id = db.Column(db.Integer, db.ForeignKey('blog_category.id'), index=True)
+    category = db.relationship(lambda: BlogCategory, backref=db.backref('posts', lazy='dynamic'))
 
     def __repr__(self):
         return self.title
@@ -41,7 +46,12 @@ class BlogComment(db.Model):
     author_email = db.Column(db.String(128))
     content = db.Column(db.Text)
 
-    post_id = db.Column(db.Integer, db.ForeignKey(BlogPost.id))
+    by_author = db.Column(db.Boolean, default=False)
+
+    replied_to_id = db.Column(db.Integer, db.ForeignKey('blog_comment.id'), index=True)
+    replied_to = db.relationship(lambda: BlogComment,
+                                 remote_side=id,
+                                 backref=db.backref('sub_comments', lazy='dynamic'))
+
+    post_id = db.Column(db.Integer, db.ForeignKey(BlogPost.id), index=True)
     post = db.relationship(BlogPost, backref=db.backref('comments', lazy='dynamic'))
-
-
