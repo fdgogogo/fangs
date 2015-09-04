6835cee79e03e952649126f648a4010f98cd3494
Fang Jiaan <fduodev@gmail.com>
2015-09-04 23:22:07 +0800

    blog models

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,34 @@
 # -*- coding: utf-8 -*-
 __author__ = 'fdgogogo'
 
+from backend import db
+
+
+class BlogCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), unique=True)
+    order = db.Column(db.SmallInteger, default=0)
+
+    __mapper_args__ = {
+        'order_by': order
+    }
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), unique=True)
+    content = db.Column(db.Text())
+    created = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+    modified = db.Column(db.DateTime, server_default=db.func.now())
+
+    category_id = db.Column(db.Integer, db.ForeignKey(BlogCategory.id))
+    category = db.relationship(BlogCategory, backref=db.backref('posts', lazy='dynamic'))
+#
+
+class BlogComment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+    modified = db.Column(db.DateTime, server_default=db.func.now())
+
+    post_id = db.Column(db.Integer, db.ForeignKey(BlogPost.id))
+    post = db.relationship(BlogPost, backref=db.backref('comments', lazy='dynamic'))
--- shelltools.py
+++ shelltools.py
@@ -1,5 +1,7 @@
 from backend import app, db
 
+from backend.blog.models import BlogCategory, BlogPost, BlogComment
+
 app.config.from_object('backend.config.DevelopmentConfig')
 ctx = app.test_request_context()
 ctx.push()
