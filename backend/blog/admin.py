0db3f012eba28026cc641a5e9c9c6ea758bf3d2d
Fang Jiaan <fduodev@gmail.com>
2015-09-05 16:27:54 +0800

    add ckeditor

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,7 +1,22 @@
 from flask.ext.admin.contrib.sqla import ModelView
+from wtforms import TextAreaField
+from wtforms.widgets import TextArea
 
 from backend import admin, db
 from backend.blog import models
+
+
+class CKTextAreaWidget(TextArea):
+    def __call__(self, field, **kwargs):
+        if kwargs.get('class'):
+            kwargs['class'] += ' ckeditor'
+        else:
+            kwargs.setdefault('class', 'ckeditor')
+        return super(CKTextAreaWidget, self).__call__(field, **kwargs)
+
+
+class CKTextAreaField(TextAreaField):
+    widget = CKTextAreaWidget()
 
 
 class BlogCategoryModelView(ModelView):
@@ -10,6 +25,13 @@ class BlogCategoryModelView(ModelView):
 
 class BlogPostModelView(ModelView):
     column_exclude_list = ('content', 'slug')
+    column_filters = ('category',)
+    form_overrides = {
+        'content': CKTextAreaField
+    }
+    create_template = 'admin/ckeditor.html'
+    edit_template = 'admin/ckeditor.html'
+    inline_models = [models.BlogComment, ]
 
 
 class BlogCommentModelView(ModelView):
--- backend/config.py
+++ backend/config.py
@@ -12,6 +12,7 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://test:test@localhost/fangs'
     DEBUG = True
     SECRET_KEY = 'xaeY\xcc\xfd\x8bS\x1d\xe8W\xe6\xc6#\xd90\xb7\xfa\xcc\x94\xc3y!*\xfe\xc0'
+    STATIC_FOLDER = 'static'
 
 
 class TestingConfig(Config):
