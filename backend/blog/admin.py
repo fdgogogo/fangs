2ac95af364fd177de99c0d1af9f5787b05d01482
Fang Jiaan <fduodev@gmail.com>
2015-09-05 16:10:05 +0800

    admin columns tweak

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,8 +1,21 @@
 from flask.ext.admin.contrib.sqla import ModelView
-from backend import admin, db
 
+from backend import admin, db
 from backend.blog import models
 
-admin.add_view(ModelView(models.BlogCategory, db.session))
-admin.add_view(ModelView(models.BlogPost, db.session))
-admin.add_view(ModelView(models.BlogComment, db.session))
+
+class BlogCategoryModelView(ModelView):
+    column_exclude_list = ('slug',)
+
+
+class BlogPostModelView(ModelView):
+    column_exclude_list = ('content', 'slug')
+
+
+class BlogCommentModelView(ModelView):
+    column_exclude_list = ('content',)
+
+
+admin.add_view(BlogCategoryModelView(models.BlogCategory, db.session))
+admin.add_view(BlogPostModelView(models.BlogPost, db.session))
+admin.add_view(BlogCommentModelView(models.BlogComment, db.session))
